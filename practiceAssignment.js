ddeb9b6448c7d44ee35be738a2c5f862986c257f
Dmccottrell <Darryl.mccottrell@outlook.com>
2024-12-03 14:03:10 -0600

    challenge1

--- practiceAssignment.js
+++ practiceAssignment.js
@@ -5,7 +5,6 @@
 
     Call this function 3 times, with input data for 3 different countries. Store the returned values in 3 different variables, and log them to the console.
 */
-
 function describeCountry(country, population, capitalCity) {
 	const people = `${country} has ${population} million people and its capital city is ${capitalCity}  `;
 	return people;
@@ -16,3 +15,78 @@ const descItaly = describeCountry("Italy", 175, "Rome");
 const descFrance = describeCountry("France", 50, "Paris");
 
 console.log(descSouth, descItaly, descFrance);
+
+//I created this function on my own
+function sportsTeam(city, football, baseball, basketball) {
+	const team = `In the ${city}, my sports teams are ${football} for football.
+        ${baseball} for baseball. And ${basketball} for basketball`;
+	return team;
+}
+
+sportsTeam("Chicago", "Bears", "Cubs", "Bulls");
+sportsTeam("Milwaukee", "Packers", "Brewers", "Bucks");
+
+console.log(sportsTeam);
+
+//FUNCTION DECLARATIONS VS EXPRESSIONS
+
+/* The world population is 7900 million people. Create a function declaration called percentageOfWorld1 which receives a population value, and returns the percentage of the world population that the given population represents. For example, China has 1441 million people, so it's about 18.2% of the world population.
+
+To calculate the percentage, divide the given population value by 7900 and then multiply by 100.
+
+Call percentageOfWorld1 for 3 populations of countries of your choice, store the results into variables, and log them to the console.
+
+Create a function expression which does the exact same thing, called percentageOfWolrd2, and also call it with 3 country populations (can be the same populations).
+
+*/
+//Function declaration
+// function percentageOfWorld1(population) {
+// 	return (population / 7900) * 100;
+// }
+
+//Function Expression
+const percentageOfWorld2 = function (population) {
+	return (population / 7900) * 100;
+};
+
+//Calling function
+const world1 = percentageOfWorld1(334);
+const world2 = percentageOfWorld1(126);
+const world3 = percentageOfWorld1(1114);
+
+console.log(world1, world2, world3);
+
+//Arrow functions
+
+//Recreate the last assignment, but this time create an arrow function called percentageOfWorld3.
+
+const percentageOfWorld3 = (population) => (population / 7900) * 100;
+const percUS = percentageOfWorld3(334);
+const percFrance = percentageOfWorld1(126);
+const percChina = percentageOfWorld1(1114);
+
+console.log(percUS, percChina, percFrance);
+
+//CALLING FUNCTIONS WITHING FUNCTION
+
+/* Create a function called describePopulation. Use the function type you like the most. This function takes in two arguments: country and population, and returns a strings like this: 'China has 1441 million people, which is about 18.2% of the world'.
+
+To calculate the percentage, describePopulation calls the percentageOfWorld1 you created earlier.
+
+Call describePopulation with data for 3 countries of your choice.
+
+*/
+
+function percentageOfWorld1(population) {
+	return (population / 7900) * 100;
+}
+
+const describePopulation = function (country, population) {
+	const percentage = percentageOfWorld1(population);
+	const description = `${country} has ${population} million people, which is about ${percentage}% of the world.`;
+	console.log(description);
+};
+
+describePopulation("Portugal", 10);
+describePopulation("China", 1441);
+describePopulation("USA", 334);
--- script.js
+++ script.js
@@ -7,6 +7,8 @@
 //Functions are a piece of code that can be reused many times in code
 //Helps keep our code dry since it is reusable code blocks
 
+/*
+//Function Declaration
 function logger() {
 	console.log("My name is Darryl");
 }
@@ -27,3 +29,103 @@ console.log(appleJuice); // console.log is a built in function
 
 const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
+
+
+//Funtion declaration
+function calcAge1(birthYear) {
+	return 2024 - birthYear;
+}
+
+const age1 = calcAge1(1999);
+console.log(age1);
+
+//Function expression
+const calcAge2 = function (birthYear) {
+	//This is an anonymous function
+	return 2024 - birthYear;
+};
+
+const age2 = calcAge2(1999);
+
+console.log(age1, age2);
+
+
+
+//Arrow Function:
+
+//Arrow function
+const calcAge3 = (birthYear) => 2024 - birthYear;
+const age3 = calcAge3(1999);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+	// multiple parameters with need
+	// ig you have more than one line have to return value
+	const age = 2024 - birthYear;
+	const retirement = 65 - age;
+	//return retirement;
+	return `${firstName} retires in ${retirement} years`;
+};
+
+console.log(yearsUntilRetirement(1999, "Darryl"));
+console.log(yearsUntilRetirement(2000, "Julia"));
+
+
+
+//Functions calling other Functions
+
+function cutFruitPieces(fruit) {
+	return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+	const applePieces = cutFruitPieces(apples);
+	const orangePieces = cutFruitPieces(oranges);
+
+	const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of oranges.`;
+	return juice; // juice will become result of running function
+}
+
+console.log(fruitProcessor(2, 3));
+
+
+const calcAge = function (birthYear) {
+	// local variable to each function
+	return 2024 - birthYear; // completely different variable outside of function below
+};
+
+const yearsUntilRetirement = function (birthYear, firstName) {
+	const age = calcAge(birthYear);
+	const retirement = 65 - age;
+
+	if (retirement > 0) {
+		console.log(`${firstName} retires in ${retirement} years`);
+		return retirement;
+	} else {
+		console.log(`${firstName} has already retired`);
+		return -1;
+	}
+};
+
+console.log(yearsUntilRetirement(1999, "Darryl"));
+console.log(yearsUntilRetirement(1935, "Mike"));
+*/
+
+//Introduction to Arrays
+
+//Arrays are data structures
+
+const friend1 = "Michael";
+const friend2 = "Peter";
+const friend3 = "Steven";
+
+//Array version of above
+
+const friends = ["Michael", "Steve", "Peter"];
+
+console.log(friends);
+
+const years = new Array(1999, 2003, 2005);
+console.log(years);
+
+console.log(friends[0]);
